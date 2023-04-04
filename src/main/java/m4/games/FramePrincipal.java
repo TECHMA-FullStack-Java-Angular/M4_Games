33ce46d375c0a94e17c5be3c5d6e1a9a5ec21d92
Paula Sousa <paulasousa137@gmail.com>
2023-04-04 13:06:08 +0200

    Actualización

--- src/main/java/m4/games/FramePrincipal.java
+++ src/main/java/m4/games/FramePrincipal.java
@@ -31,10 +31,8 @@ public class FramePrincipal extends JFrame {
 	private JTextField textJug2;
 	private ButtonGroup jugador = new ButtonGroup();
 	JLabel lblAccion = new JLabel();
-	
+
 	private int turn = 0;
-
-
 
 	/**
 	 * Create the frame.
@@ -48,171 +46,160 @@ public class FramePrincipal extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
-		
-		lblAccion = new JLabel("Bienvenido al 3 en raya!");
+
+		lblAccion = new JLabel("Introduzca el nombre de los jugadores");
 		lblAccion.setBounds(427, 80, 226, 14);
 		contentPane.add(lblAccion);
-//		completarAccion(lblAccion);
-		lblAccion.setText("Introduzca el nombre de los jugadores");
 		
+
 		textJug1 = new JTextField();
 		textJug1.setBounds(510, 162, 96, 20);
 		contentPane.add(textJug1);
 		textJug1.setColumns(10);
-		
+
 		textJug1.getDocument().addDocumentListener(dL);
-	
-		
-		
-		
-		
-		
-		
+
 		JToggleButton tglbtn = new JToggleButton("");
 		tglbtn.setBounds(60, 40, 90, 90);
 		contentPane.add(tglbtn);
 		xo(tglbtn, lblAccion);
-		
+
 		JToggleButton tglbtn_1 = new JToggleButton("");
 		tglbtn_1.setBounds(155, 40, 90, 90);
 		contentPane.add(tglbtn_1);
 		xo(tglbtn_1, lblAccion);
-		
+
 		JToggleButton tglbtn_2 = new JToggleButton("");
 		tglbtn_2.setBounds(250, 40, 90, 90);
 		contentPane.add(tglbtn_2);
 		xo(tglbtn_2, lblAccion);
-		
+
 		JToggleButton tglbtn_3 = new JToggleButton("");
 		tglbtn_3.setBounds(60, 135, 90, 90);
 		contentPane.add(tglbtn_3);
 		xo(tglbtn_3, lblAccion);
-		
+
 		JToggleButton tglbtn_4 = new JToggleButton("");
 		tglbtn_4.setBounds(155, 135, 90, 90);
 		contentPane.add(tglbtn_4);
 		xo(tglbtn_4, lblAccion);
-		
+
 		JToggleButton tglbtn_5 = new JToggleButton("");
 		tglbtn_5.setBounds(250, 135, 90, 90);
 		contentPane.add(tglbtn_5);
 		xo(tglbtn_5, lblAccion);
-		
+
 		JToggleButton tglbtn_6 = new JToggleButton("");
 		tglbtn_6.setBounds(60, 230, 90, 90);
 		contentPane.add(tglbtn_6);
 		xo(tglbtn_6, lblAccion);
-		
+
 		JToggleButton tglbtn_7 = new JToggleButton("");
 		tglbtn_7.setBounds(155, 230, 90, 90);
 		contentPane.add(tglbtn_7);
 		xo(tglbtn_7, lblAccion);
-		
+
 		JToggleButton tglbtn_8 = new JToggleButton("");
 		tglbtn_8.setBounds(250, 230, 90, 90);
 		contentPane.add(tglbtn_8);
 		xo(tglbtn_8, lblAccion);
-		
+
 		JButton btnNuevaPartida = new JButton("Nueva Partida");
 		btnNuevaPartida.addActionListener(aL);
 		btnNuevaPartida.setBounds(505, 25, 119, 23);
 		contentPane.add(btnNuevaPartida);
-		
+
 		JLabel lblJugador1 = new JLabel("Jugador 1:");
 		lblJugador1.setBounds(427, 124, 88, 14);
 		contentPane.add(lblJugador1);
-		
+
 		JLabel lblNombre = new JLabel("");
 		lblNombre.setBounds(427, 170, 49, 14);
 		contentPane.add(lblNombre);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Nombre:");
 		lblNewLabel_1.setBounds(427, 170, 49, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JRadioButton rdbtnHumano = new JRadioButton("Humano");
 		rdbtnHumano.setBounds(427, 218, 111, 23);
 		contentPane.add(rdbtnHumano);
 		jugador.add(rdbtnHumano);
-		
+
 		JRadioButton rdbtnCPU = new JRadioButton("CPU");
 		rdbtnCPU.setBounds(567, 218, 111, 23);
 		contentPane.add(rdbtnCPU);
 		jugador.add(rdbtnCPU);
-		
+
 		JLabel lblJugador2 = new JLabel("Jugador 2:");
 		lblJugador2.setBounds(427, 284, 88, 14);
 		contentPane.add(lblJugador2);
-		
+
 		JLabel lblNombre_2 = new JLabel("Nombre:");
 		lblNombre_2.setBounds(427, 309, 49, 14);
 		contentPane.add(lblNombre_2);
-		
+
 		textJug2 = new JTextField();
 		textJug2.setColumns(10);
 		textJug2.setBounds(505, 309, 96, 20);
 		contentPane.add(textJug2);
-		
+
 		JRadioButton rdbtnHumano_1 = new JRadioButton("Humano");
 		rdbtnHumano_1.setBounds(427, 359, 111, 23);
 		contentPane.add(rdbtnHumano_1);
-		
+
 		JRadioButton rdbtnCPU_1 = new JRadioButton("CPU");
 		rdbtnCPU_1.setBounds(542, 359, 111, 23);
 		contentPane.add(rdbtnCPU_1);
-		
-		
-		
-		
+
 		setVisible(true);
 	}
 
-	ActionListener aL =new ActionListener() {
+	ActionListener aL = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
-			
-			
+
 		}
 	};
-	
+
 	public void xo(JToggleButton b, JLabel l) {
-		b.addItemListener(new ItemListener() {			
+		b.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				b.setFont(new Font("Arial", Font.BOLD, 70));
-				if(b.isSelected()) {
-					if (turn%2 == 0) {
-						l.setText("Turno de jugador "+ textJug2.getText());
+				if (b.isSelected()) {
+					if (turn % 2 == 0) {
+						l.setText("Turno de jugador " + textJug2.getText());
 						b.setText("X");
 					} else {
-						l.setText("Turno de jugador "+ textJug1.getText());
+						l.setText("Turno de jugador " + textJug1.getText());
 						b.setText("O");
 					}
 				}
 				b.setEnabled(false);
 				turn++;
-				
+
 			}
 		});
 	}
-	
+
 	DocumentListener dL = new DocumentListener() {
-	    @Override
-	    public void changedUpdate(DocumentEvent e) {
-	        updateLabel();
-	    }
-	    @Override
-	    public void insertUpdate(DocumentEvent e) {
-	        updateLabel();
-	    }
-	    @Override
-	    public void removeUpdate(DocumentEvent e) {
-	        updateLabel();
-	    }
-	    
-	    private void updateLabel() {
-	        lblAccion.setText("Empieza el/la jugador/a: "+textJug1.getText());
-	    }
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			updateLabel();
+		}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			updateLabel();
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			updateLabel();
+		}
+
+		private void updateLabel() {
+			lblAccion.setText("Empieza el/la jugador/a: " + textJug1.getText());
+		}
 	};
 }
