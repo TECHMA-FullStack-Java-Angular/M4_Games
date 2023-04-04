007dc5a3f9b4867573a93a41692ba7450c91654b
Paula Sousa <paulasousa137@gmail.com>
2023-04-04 12:22:16 +0200

    Nombre dinámico

--- src/main/java/m4/games/FramePrincipal.java
+++ src/main/java/m4/games/FramePrincipal.java
@@ -19,6 +19,9 @@ import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JInternalFrame;
 import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.awt.Color;
 
 public class FramePrincipal extends JFrame {
@@ -27,6 +30,7 @@ public class FramePrincipal extends JFrame {
 	private JTextField textJug1;
 	private JTextField textJug2;
 	private ButtonGroup jugador = new ButtonGroup();
+	JLabel lblAccion = new JLabel();
 	
 	private int turn = 0;
 
@@ -45,14 +49,26 @@ public class FramePrincipal extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		
+		
+		lblAccion = new JLabel("Bienvenido al 3 en raya!");
+		lblAccion.setBounds(427, 80, 226, 14);
+		contentPane.add(lblAccion);
+//		completarAccion(lblAccion);
+		lblAccion.setText("Introduzca el nombre de los jugadores");
+		
 		textJug1 = new JTextField();
 		textJug1.setBounds(510, 162, 96, 20);
 		contentPane.add(textJug1);
 		textJug1.setColumns(10);
 		
-		JLabel lblAccion = new JLabel("Empieza la partida, turno de " + textJug1.getText());
-		lblAccion.setBounds(427, 80, 226, 14);
-		contentPane.add(lblAccion);
+		textJug1.getDocument().addDocumentListener(dL);
+	
+		
+		
+		
+		
+		
 		
 		JToggleButton tglbtn = new JToggleButton("");
 		tglbtn.setBounds(60, 40, 90, 90);
@@ -180,4 +196,23 @@ public class FramePrincipal extends JFrame {
 			}
 		});
 	}
+	
+	DocumentListener dL = new DocumentListener() {
+	    @Override
+	    public void changedUpdate(DocumentEvent e) {
+	        updateLabel();
+	    }
+	    @Override
+	    public void insertUpdate(DocumentEvent e) {
+	        updateLabel();
+	    }
+	    @Override
+	    public void removeUpdate(DocumentEvent e) {
+	        updateLabel();
+	    }
+	    
+	    private void updateLabel() {
+	        lblAccion.setText("Empieza el/la jugador/a: "+textJug1.getText());
+	    }
+	};
 }
