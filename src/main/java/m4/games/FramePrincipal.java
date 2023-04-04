9c5ce3dd96e587b6e6a6359858999a76b2edbcd8
Paula Sousa <paulasousa137@gmail.com>
2023-04-04 13:50:48 +0200

    Activar, actualizar y desactivar botones

--- src/main/java/m4/games/FramePrincipal.java
+++ src/main/java/m4/games/FramePrincipal.java
@@ -16,10 +16,13 @@ import javax.swing.JRadioButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JInternalFrame;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.JSeparator;
 
 public class FramePrincipal extends JFrame {
@@ -27,7 +30,10 @@ public class FramePrincipal extends JFrame {
 	private JPanel contentPane;
 	private JTextField textJug1;
 	private JTextField textJug2;
-	private ButtonGroup jugador = new ButtonGroup();
+	private ButtonGroup jugador1 = new ButtonGroup();
+	private ButtonGroup jugador2 = new ButtonGroup();
+	private ArrayList<JToggleButton> listaBotones= new ArrayList<JToggleButton>();
+	
 	
 	private int turn = 0;
 
@@ -66,46 +72,55 @@ public class FramePrincipal extends JFrame {
 		tglbtn.setBounds(60, 40, 90, 90);
 		contentPane.add(tglbtn);
 		xo(tglbtn, lblAccion);
+		listaBotones.add(tglbtn);
 		
 		JToggleButton tglbtn_1 = new JToggleButton("");
 		tglbtn_1.setBounds(155, 40, 90, 90);
 		contentPane.add(tglbtn_1);
 		xo(tglbtn_1, lblAccion);
+		listaBotones.add(tglbtn_1);
 		
 		JToggleButton tglbtn_2 = new JToggleButton("");
 		tglbtn_2.setBounds(250, 40, 90, 90);
 		contentPane.add(tglbtn_2);
 		xo(tglbtn_2, lblAccion);
-		
+		listaBotones.add(tglbtn_2);
+				
 		JToggleButton tglbtn_3 = new JToggleButton("");
 		tglbtn_3.setBounds(60, 135, 90, 90);
 		contentPane.add(tglbtn_3);
 		xo(tglbtn_3, lblAccion);
+		listaBotones.add(tglbtn_3);
 		
 		JToggleButton tglbtn_4 = new JToggleButton("");
 		tglbtn_4.setBounds(155, 135, 90, 90);
 		contentPane.add(tglbtn_4);
 		xo(tglbtn_4, lblAccion);
+		listaBotones.add(tglbtn_4);
 		
 		JToggleButton tglbtn_5 = new JToggleButton("");
 		tglbtn_5.setBounds(250, 135, 90, 90);
 		contentPane.add(tglbtn_5);
 		xo(tglbtn_5, lblAccion);
+		listaBotones.add(tglbtn_5);
 		
 		JToggleButton tglbtn_6 = new JToggleButton("");
 		tglbtn_6.setBounds(60, 230, 90, 90);
 		contentPane.add(tglbtn_6);
 		xo(tglbtn_6, lblAccion);
+		listaBotones.add(tglbtn_6);
 		
 		JToggleButton tglbtn_7 = new JToggleButton("");
 		tglbtn_7.setBounds(155, 230, 90, 90);
 		contentPane.add(tglbtn_7);
 		xo(tglbtn_7, lblAccion);
+		listaBotones.add(tglbtn_7);
 		
 		JToggleButton tglbtn_8 = new JToggleButton("");
 		tglbtn_8.setBounds(250, 230, 90, 90);
 		contentPane.add(tglbtn_8);
 		xo(tglbtn_8, lblAccion);
+		listaBotones.add(tglbtn_8);
 		
 		Game.startGame(lblAccion, tglbtn);
 		Game.buttonOff(tglbtn);
@@ -138,12 +153,12 @@ public class FramePrincipal extends JFrame {
 		JRadioButton rdbtnHumano = new JRadioButton("Humano");
 		rdbtnHumano.setBounds(427, 218, 111, 23);
 		contentPane.add(rdbtnHumano);
-		jugador.add(rdbtnHumano);
+		jugador1.add(rdbtnHumano);
 		
 		JRadioButton rdbtnCPU = new JRadioButton("CPU");
 		rdbtnCPU.setBounds(567, 218, 111, 23);
 		contentPane.add(rdbtnCPU);
-		jugador.add(rdbtnCPU);
+		jugador2.add(rdbtnCPU);
 		
 		JLabel lblJugador2 = new JLabel("Jugador 2:");
 		lblJugador2.setBounds(427, 284, 88, 14);
@@ -173,11 +188,21 @@ public class FramePrincipal extends JFrame {
 		setVisible(true);
 	}
 
+
+	
 	ActionListener aL =new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			
-			
-		}
+		  public void actionPerformed(ActionEvent e) {
+			  
+			  for(int i=0; i<listaBotones.size(); i++) {
+				  listaBotones.get(i).setSelected(false);
+				  listaBotones.get(i).setText("");
+			  }
+			  
+			  for(int i=0; i<listaBotones.size(); i++) {
+				  listaBotones.get(i).setEnabled(true);;
+			  }
+		  }
+	    
 	};
 	
 	public void xo(JToggleButton b, JLabel l) {
@@ -187,10 +212,10 @@ public class FramePrincipal extends JFrame {
 				b.setFont(new Font("Arial", Font.BOLD, 70));
 				if(b.isSelected()) {
 					if (turn%2 == 0) {
-						l.setText("Turno de jugador 2");
+						l.setText("Turno de jugador "+ textJug2.getText());
 						b.setText("X");
 					} else {
-						l.setText("Turno de jugador 1");
+						l.setText("Turno de jugador "+ textJug1.getText());
 						b.setText("O");
 					}
 				}
